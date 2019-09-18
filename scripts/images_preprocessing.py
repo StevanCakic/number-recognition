450dfeb28c5804eb7aa825910d7acf5f6cf943ca
StevanCakic <stevancakic93@gmail.com>
2019-09-18 09:18:26 +0200

    some fixes

--- scripts/images_preprocessing.py
+++ scripts/images_preprocessing.py
@@ -2,10 +2,12 @@ import cv2
 import sys
 import os
 
-def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+# https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
+def image_resize(image, width=None, height=None, inter=cv2.INTER_LINEAR):
     # initialize the dimensions of the image to be resized and
     # grab the image size
     dim = None
+
     (real_height, real_width) = image.shape[:2]
 
     # if both the width and height are None, then return the
@@ -13,19 +15,10 @@ def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     if width is None and height is None:
         return image
 
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        ratio = height / float(real_height)
-        dim = (int(real_width * ratio), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        ratio = width / float(real_width)
-        dim = (width, int(real_height * ratio))
+    # calculate the ratio of the height and construct the
+    # dimensions
+    ratio = height / float(real_height)
+    dim = (int(real_width * ratio), height)
 
     # resize the image
     resized = cv2.resize(image, dim, interpolation=inter)
@@ -41,30 +34,34 @@ def main(images):
         output_location = root_folder + "\\preprocessed\\" + filename
 
         # read image
+        # images - folder where all images are stored
+        # filename - specific image in images folder
+
         img = cv2.imread(f'{images}\\{filename}')
         # cv.imshow("Original image", img)
 
         # resized image
-        resized_img = image_resize(img, 500, 100)
+        resized_img = image_resize(img, height=130)
         # cv2.imshow("Original resized image", resized_img)
 
         # Convert image to gray
-        img_gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
-        # cv2.imshow("Gray image", img_gray)
+        gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow("Gray image", gray)
 
         # Get resized image dimensions
-        rows, _ = img_gray.shape
+        rows, _ = gray.shape
 
         # Get horizontal size (for creating kernel to remove horizontal lines)
         horizontalsize = int(rows / 35)
 
         # Creating kernel for dilate
         # MOTPH_ELLIPSE works best
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, horizontalsize))
+        ksize = (3, horizontalsize)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize)
 
         # Create dilate image
         # Docs: https://docs.opencv.org/3.4/db/df6/tutorial_erosion_dilatation.html
-        dilated_image = cv2.dilate(img_gray, kernel)
+        dilated_image = cv2.dilate(gray, kernel)
         # cv2.imshow("Dilate", dilated_image)
         
         # Blured by Gaussian blur
